a0ee232489587b47d0347d279b3368e306a4d2c1
nobabsarkar <nobabsarkar2016@gmail.com>
2025-04-02 15:54:12 +0600

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h1>This is about page component</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/found-items/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h1>This is found items page component</h1>
+    </div>
+  );
+};
+
+export default page;
