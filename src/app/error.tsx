ae6913c8b778f4093570413e07d90d6a92d30052
nobabsarkar <nobabsarkar2016@gmail.com>
2025-07-30 23:53:24 +0600

    change code

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -19,7 +19,7 @@ export default function Error({
       <h2>Something went wrong!</h2>
       <button
         onClick={
-          // Attempt to recover by trying to re-render the segment
+          // Attempt to recover by trying to re-render the segment here
           () => reset()
         }
       >
